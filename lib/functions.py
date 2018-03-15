95def649a07badbdd9578b839f4550782f673ae4
Justin Tatman <jatatman12@gmail.com>
2018-03-14 20:08:49 -0600

    broncos model

--- lib/functions.py
+++ lib/functions.py
@@ -9,23 +9,53 @@ class FoozBall():
 
 
 	def data_info():
-		print('''
+		print(
+		'''
 			Columns
 			_______
-			Year
-			W
-			L
-			T
-			PF
-			PA
-			PD
-			Pts
-			Yds
+			Yr   - Year
+			Lg   - League
+			Tm   - Team
+			W    - Wins
+			L    - Losses
+			T    - Ties
+			DF   - Division Finish
+			PO   - Playoffs
+			PF   - Points For
+			PA   - Points Against
+			PD   - Point Differential
+			HDC  - Head Coach
+			AV   - Approximate Value / Most valuable Player
+			PAS  - Top Passer
+			RSH  - Top Rusher
+			REC  - Top Receiver
+			OPR  - Offensive Point Ranking
+			OYR  - Offensive Yard Ranking
+			DPR  - Deffensive Point Ranking
+			DYR  - Deffensive Yard Ranking
+			T/G  - Takeaway / Giveaway ratio ranking
+			PDR  - Point Differential Ranking
+			YDR  - Yard Differential Ranking
+			TT   - Total Teams
+			MoV  - Margin of Victory
+			SoS  - Strength of Schedule
+			SRS  - Simple Rating System
+			OSRS - Offensive quality relative to average (SRS)
+			DSRS - Defensive quality relative to average (SRS)
 
-			''')
+			Simple Rating System
+			____________________
+			SRS = MoV + SoS = OSRS + DSRS
+		'''
+			)
 
 
 	def get_data(self, url):
+		'''
+		Requests data from a website using requests.
+		Html data is parsed using BeautifulSoup.
+		Returns data as a pandas data frame.
+		'''
 
 		r = requests.get(url)
 		content = r.content
@@ -38,6 +68,7 @@ class FoozBall():
 		year = []
 		data = []
 
+		# extracting data from html table
 		for i in range(2,len(table_soup)):
 			y = [th.get_text() for th in table_soup[i].findAll('th')]
 			d = [td.get_text() for td in table_soup[i].findAll('td')]
@@ -45,12 +76,30 @@ class FoozBall():
 			data.append(d)
 
 		year = pd.Series(year, name='year')
-		data = pd.DataFrame(data, columns =cols[1:])
+		data = pd.DataFrame(data, columns = cols[1:])
 		self.raw_data = pd.concat((year, data), axis=1)
 
-		return self
+		return self.raw_data
+
+	def column_clean(self):
+		'''
+		Condensing two header columns into one.
+		'''
+
+		columns = [
+		'Yr', 'Lg', 'Tm', 'W', 'L', 'T', 'DF', 'PO', 'PF',\
+		'PA','PD','HDC', 'AV', 'PAS', 'RSH', 'REC', 'OPR', 'OYR',\
+		'DPR', 'DYR', 'T/G', 'PDR', 'YDR', 'TT', 'MoV', 'SoS',\
+		'SRS', 'OSRS', 'DSRS'
+		]
+
+		self.raw_data.columns = columns
+
+		print('Columns have been cleaned.')
+
 
 	def clean(self, num_only=False):
+
 
 		# Table data on the webpage has breaks
 		# Using a mask to find the null values that are the table break
@@ -59,30 +108,8 @@ class FoozBall():
 		break_rows = [row for row in breaks]
 		data = self.raw_data.drop(break_rows,axis=0)
 
-		columns = ['Year', 'League', 'Team', 'Div_Finish', 'Playoffs', 'Points_For', 'Points_Against', 'Point_Difference',
-		'Coaches', 'MVP', 'Passer', 'Rusher', "Reciever', "]
-
-
-
 		# Removing the year values from the nested list
-		
-		data['year'] = data['year'].apply(lambda x: int(x[0]))
 
-		str_cols = ['Lg', 'Tm', 'Div. Finish', \
-					'Playoffs', 'Coaches', 'AV', \
-					'Passer', 'Rusher', 'Receiver'
-					]
-		num_cols = [col for col in data.columns if col not in str_cols]
+		data['Yr'] = data['Yr'].apply(lambda x: int(x[0]))
 
-
-		str_df = data[str_cols]
-		num_df = data[num_cols]
-		num_df = num_df.applymap(lambda x: float(x))
-
-		if num_only:
-			return num_df
-		else:
-			return pd.concat((num_df,str_df))
-
-
-
+		return data
